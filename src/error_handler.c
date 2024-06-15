cf8a098213aaa82c34cc27f64bd7640224696b51
StjepanPrakljacic <stjepanprakljacic@gmail.com>
2024-06-15 16:35:40 +0200

    Adding more error code values.

--- src/error_handler.c
+++ src/error_handler.c
@@ -17,6 +17,36 @@ void log_error_message(ErrorCode code) {
         case ERROR_FILE_OPEN:
         Log_AddMsg(ERROR, "Failed to open a file.");
         break;
+         case ERROR_FILE_CLOSE:
+            Log_AddMsg(ERROR, "Failed to close a file.");
+            break;
+        case ERROR_FILE_READ:
+            Log_AddMsg(ERROR, "Failed to read from a file.");
+            break;
+        case ERROR_FILE_WRITE:
+            Log_AddMsg(ERROR, "Failed to write to a file.");
+            break;
+        case ERROR_FILE_DELETE:
+            Log_AddMsg(ERROR, "Failed to delete a file.");
+            break;
+        case ERROR_FILE_NOT_FOUND:
+            Log_AddMsg(ERROR, "File not found.");
+            break;
+        case ERROR_INVALID_FILENAME:
+            Log_AddMsg(ERROR, "Invalid filename provided.");
+            break;
+        case ERROR_INVALID_INPUT:
+            Log_AddMsg(ERROR, "Invalid input from the user.");
+            break;
+        case ERROR_INVALID_COMMAND:
+            Log_AddMsg(ERROR, "Invalid command entered.");
+            break;
+        case ERROR_FILE_ALREADY_EXISTS:
+            Log_AddMsg(ERROR, "File already exists.");
+            break;
+        case ERROR_MAX_FILES_REACHED:
+            Log_AddMsg(ERROR, "Maximum number of files reached.");
+            break;
         default:
         Log_AddMsg(ERROR, "Unknown error.");
         break;
