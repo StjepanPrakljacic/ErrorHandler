cea47bf5d9fadc312d9895a186b4d04d5818d53b
StjepanPrakljacic <stjepanprakljacic@gmail.com>
2024-06-15 16:37:10 +0200

    Adding more error code values.

--- src/error_handler.c
+++ src/error_handler.c
@@ -3,21 +3,21 @@
 void log_error_message(ErrorCode code) {
     switch (code) {
         case ERROR_MEMORY_ALLOCATION:
-        Log_AddMsg(ERROR, "Memory allocation error.");
-        break;
+            Log_AddMsg(ERROR, "Memory allocation error.");
+            break;
         case ERROR_BUFFER_OVERFLOW:
-        Log_AddMsg(ERROR, "Buffer overflow error.");
-        break;
+            Log_AddMsg(ERROR, "Buffer overflow error.");
+            break;
         case ERROR_INVALID_TASK:
-        Log_AddMsg(ERROR, "Invalid task.");
-        break;
+            Log_AddMsg(ERROR, "Invalid task.");
+            break;
         case ERROR_TASK_NOT_FOUND:
-        Log_AddMsg(ERROR, "Task not found.");
-        break;
+            Log_AddMsg(ERROR, "Task not found.");
+            break;
         case ERROR_FILE_OPEN:
-        Log_AddMsg(ERROR, "Failed to open a file.");
-        break;
-         case ERROR_FILE_CLOSE:
+            Log_AddMsg(ERROR, "Failed to open a file.");
+            break;
+        case ERROR_FILE_CLOSE:
             Log_AddMsg(ERROR, "Failed to close a file.");
             break;
         case ERROR_FILE_READ:
@@ -48,7 +48,7 @@ void log_error_message(ErrorCode code) {
             Log_AddMsg(ERROR, "Maximum number of files reached.");
             break;
         default:
-        Log_AddMsg(ERROR, "Unknown error.");
-        break;
+            Log_AddMsg(ERROR, "Unknown error.");
+            break;
     }
 }
